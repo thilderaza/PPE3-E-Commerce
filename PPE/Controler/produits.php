ad163577a303e19650a2b5df62914c6b963ec40d
thilderaza <54800952+thilderaza@users.noreply.github.com>
2019-12-10 15:55:14 +0400

    Update produits.php

--- PPE/Controler/produits.php
+++ PPE/Controler/produits.php
@@ -20,16 +20,15 @@ class Produits extends DB {
     return $del;
   }
 
-  function setAdd($tblprod){
+  function setAdd($tblemp){
 
-    $strSQL = "INSERT INTO produit (num_prod,nom_prod,prix,type_prod,categ_prod,descrip_prod) VALUES (UCASE(?),UCASE(?), UCASE(?), UCASE(?), UCASE(?), UCASE(?) )";
+    $strSQL = "INSERT INTO produit (nom_prod,prix,type_prod,categ_prod,descrip_prod) VALUES (?,?,?,?,?)";
     $tabValeur = array(
-      $tblemp['num'],
-      $tblemp['nom'],
+      $tblemp['nom_prod'],
       $tblemp['prix'],
-      $tblemp['type'],
-      $tblemp['categ'],
-      $tblemp['descrip']
+      $tblemp['type_prod'],
+      $tblemp['categ_prod'],
+      $tblemp['descrip_prod']
     );
     $ins = $this->Requete($strSQL, $tabValeur);
     return $ins;
@@ -38,16 +37,15 @@ class Produits extends DB {
 
   function setUpdate($tblemp){
 
-    $strSQL = "UPDATE produit SET nom_prod, prix , type_prod,
-               categ_prod,descrip_prod WHERE num_prod = ? ;";
+    $strSQL = "UPDATE produit SET nom_prod = :nom_prod, prix = :prix, type_prod = :type_prod, categ_prod = :categ_prod, descrip_prod = :descrip_prod WHERE num_prod = :num_prod; ";
 
     $tabValeur = array(
       'nom_prod'  => $tblemp['nom_prod'], 
-      'prix'   => $tblemp['prix'], 
-      'type_prod'   => $tblemp['type_prod'],
-      'categ_prod'   => $tblemp['categ_prod'],
-      'descrip_prod'   => $tblemp['descrip_prod'],
-      'num_prod'   => $tblemp['ide'],
+      'prix'      => $tblemp['prix'], 
+      'type_prod' => $tblemp['type_prod'],
+      'categ_prod'=> $tblemp['categ_prod'],
+      'descrip_prod'=> $tblemp['descrip_prod'],
+      'num_prod'   => $tblemp['num_prod'],
     );
     
     $upd = $this->Requete($strSQL, $tabValeur);
@@ -55,3 +53,4 @@ class Produits extends DB {
   }
 }
 ?>
+
